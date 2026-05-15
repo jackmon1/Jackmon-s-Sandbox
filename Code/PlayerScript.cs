617a5612614d43db928814f1046371dd00226afb
jackmon1 <moranjack44@gmail.com>
2026-05-15 12:03:16 -0500

    Created downtown testing scene

--- Code/PlayerScript.cs
+++ Code/PlayerScript.cs
@@ -1,9 +1,28 @@
 using Sandbox;
+using System.Linq;
 
 public sealed class PlayerScript : Component
 {
+
+
 	protected override void OnUpdate()
 	{
+		if (IsProxy) 
+			return;
 
+		if ( !Input.AnalogMove.IsNearZeroLength )
+		{
+			WorldPosition += Input.AnalogMove.Normal * Time.Delta * 100.0f;	
+		}
+
+		var camera = Scene.GetAllComponents<CameraComponent>().FirstOrDefault();
+		camera.WorldRotation = new Angles( 45, 0, 0 );
+		camera.WorldPosition  = WorldPosition  + camera.WorldRotation.Backward * 1500;
+
+
+
+
+
+		
 	}
 }
